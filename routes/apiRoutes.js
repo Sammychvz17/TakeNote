5e7763c3acb0b15f6dcbc47b7bd3296855ce1d5d
sammychvz <sammy_chvz17@outlook.com>
2022-02-21 19:58:48 -0600

    added the get routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,13 @@
 const db = require("../db/db.json"); 
 const path = require("path"); 
-const fs = require("fs")
+const fs = require("fs");
+const { application } = require("express");
 
+module.exports = function (app) {
+    app.get("/api/notes", (req, res) => {
+        fs.readFile("./db/db.json", (err, notesAdd) => {
+            if (err) throw err; 
+            res.json(JSON.parse(notesAdd));
+        })
+    });
+};
