9d12b9240a3fdcb91e98dc98f990ac2e8c762ef6
sammychvz <sammy_chvz17@outlook.com>
2022-02-21 20:16:54 -0600

    added the post and delete routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,46 @@
 const db = require("../db/db.json"); 
 const path = require("path"); 
 const fs = require("fs");
-const { application } = require("express");
+
 
 module.exports = function (app) {
     app.get("/api/notes", (req, res) => {
         fs.readFile("./db/db.json", (err, notesAdd) => {
             if (err) throw err; 
             res.json(JSON.parse(notesAdd));
-        })
+        });
     });
+
+
+app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err; 
+        const notesData = JSON.parse(data); 
+        notesData.push(req.body); 
+        for (let i = 0; i < notesData.length; i++) {
+            notesData[i].id = i + 1;
+          }
+          fs.writeFile("./db/db.json", JSON.stringify(notesData), (err) => {
+            if (err) throw err; 
+            res.send(db);
+          });
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      let newNote = JSON.parse(data);
+      for (let i = 0; i < newNote.length; i++) {
+        if (newNote[i].id == req.params.id) {
+          newNote.splice(i, 1);
+        }
+      }
+
+      fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
+        if (err) throw err;
+        res.send(newNote);
+      });
+    });
+  });
 };
