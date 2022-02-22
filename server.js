82d58bc9a021b211c423b040dd321eb2a3751b63
sammychvz <sammy_chvz17@outlook.com>
2022-02-21 19:45:12 -0600

    added the express parse and the port and connected routes to server js

--- server.js
+++ server.js
@@ -3,5 +3,20 @@ const path = require("path");
 
 const app = express();
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
 const PORT = process.env.PORT || 3001; 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
 
